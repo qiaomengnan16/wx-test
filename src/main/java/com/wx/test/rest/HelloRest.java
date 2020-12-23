35237498798c5af6b461cdcee5d13be64ba97729
qiaomengnan <285921957@qq.com>
2020-12-23 16:21:21 +0800

    'xxx'

---
+++ src/main/java/com/wx/test/rest/HelloRest.java
@@ -0,0 +1,15 @@
+package com.wx.test.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRest {
+
+    @RequestMapping(value = "hello",method = RequestMethod.GET)
+    public String hello(){
+        return "hello";
+    }
+
+}
